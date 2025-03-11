52be6303266b218c5acca4eafc87492c3c1f4d80
ishasingh97 <150110262+ishasingh97@users.noreply.github.com>
2025-03-12 00:56:52 +0530

    removed unused config

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,8 +5,5 @@ export default defineConfig({
     baseUrl: 'http://localhost:8080',
     supportFile: false,
     video: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
